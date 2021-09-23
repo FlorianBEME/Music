498bc57ef896a352e1b07edb80fda2b746bcfe2d
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-09-23 14:49:46 +0200

    Update AdminNavBar.jsx

--- client/src/components/admin/AdminNavBar.jsx
+++ client/src/components/admin/AdminNavBar.jsx
@@ -59,7 +59,7 @@ const AdminNavBar = () => {
                   type="button"
                   className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  se deconecter
+                  Deconnexion
                   {/* <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
                 </button>
@@ -147,7 +147,7 @@ const AdminNavBar = () => {
                       disconect();
                     }}
                   >
-                    Disconnect
+                    Deconnexion
                   </div>
                 </div>
               </div>
