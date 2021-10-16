2c7ce5297e355c5ddd640e2cc5a8f2b75ed7e89a
FlorianBEME <florianbme@gmail.com>
2021-10-17 00:48:32 +0200

    TS migration: Accordion && adminNavBAr

--- client/src/components/common/Accordion.tsx
+++ client/src/components/common/Accordion.tsx
@@ -3,7 +3,13 @@ import { Transition } from "@tailwindui/react";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { AiOutlineArrowUp } from "react-icons/ai";
 
-function Accordion({ title, text_content, status }) {
+type AccordionProps = {
+  title: string;
+  text_content: string;
+  status: string;
+};
+
+function Accordion({ title, text_content, status }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -38,28 +44,6 @@ function Accordion({ title, text_content, status }) {
         >
           <article className="border-b dark:bg-gray-600 dark:text-gray-100">
             <div className="">
-              {/* <header className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none">
-                    <span className="text-indigo font-thin text-xl">
-                    Lorem ipsum dolor sit amet
-                    </span>
-                    <div className="rounded-full border border-indigo w-7 h-7 flex items-center justify-center bg-indigo">
-                    <svg
-                        aria-hidden="true"
-                        data-reactid="281"
-                        fill="none"
-                        height="24"
-                        stroke="white"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewbox="0 0 24 24"
-                        width="24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <polyline points="18 15 12 9 6 15"></polyline>
-                    </svg>
-                    </div>
-                </header> */}
               <div className="px-8 py-5 p text-grey-darkest">
                 <p>{text_content}</p>
               </div>
