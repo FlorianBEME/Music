648a2e67ba889c8dc7ee47978120f4af688a5c60
FlorianBEME <69846887+FlorianBEME@users.noreply.github.com>
2021-10-20 14:41:49 +0200

    Update useDarkMode.ts
    Suppression de code commenté

--- client/src/hooks/useDarkMode.ts
+++ client/src/hooks/useDarkMode.ts
@@ -1,21 +1,6 @@
 import { useState, useEffect } from "react";
 
 export default function useDarkMode() {
-  //   const themeStorage = localStorage.getItem("MusicRequestThemePreference");
-  //   const [theme, setTheme] = useState(themeStorage || "");
-  //   const colorTheme = theme === "light" ? "dark" : "light";
-
-  //   useEffect(() => {
-  //     if(themeStorage){
-  // console.log(theme);
-  //   }
-  //     console.log(theme);
-
-  //     window.document.documentElement.classList.remove(colorTheme);
-  //     window.document.documentElement.classList.add(theme);
-  //     localStorage.setItem("MusicRequestThemePreference", theme);
-  //   }, [colorTheme, theme, themeStorage]);
-
   const themeStorage = localStorage.getItem("MusicRequestThemePreference");
   const [theme, setTheme] = useState<any>(themeStorage);
   const colorTheme = theme === "light" ? "dark" : "light";
