f3189f2e273e5c8335851d5fd3952ab9b6b8db72
FlorianBEME <florianbme@gmail.com>
2021-10-22 13:25:54 +0200

    ajout suppression d'image dans le serveur lorsque l'on supprime un icone du footer

--- src/routes/footeritem.routes.js
+++ src/routes/footeritem.routes.js
@@ -1,6 +1,8 @@
 const { connection } = require("../db_connection");
 const router = require("express").Router();
 const { verifyJWT } = require("../middlewares/isuserauth.js");
+const front = `${__dirname}/../../client/build`;
+const fs = require("fs");
 
 router.get("/", (req, res) => {
   const sql = "SELECT * FROM footer_item";
@@ -51,7 +53,14 @@ router.delete("/:id", verifyJWT, (req, res) => {
     if (err) {
       res.status(500).send({ errorMessage: err.message });
     } else {
-      res.sendStatus(200);
+      // Suppression de l'image
+      fs.unlink(`${front}${req.body.path}`, (err) => {
+        if (err) {
+          return res.status(500).send({ errorMessage: err.message });
+        } else {
+          res.sendStatus(200);
+        }
+      });
     }
   });
 });
