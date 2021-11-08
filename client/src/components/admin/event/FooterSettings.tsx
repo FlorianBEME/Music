2e25e8b7b1f63f268b375b20d31d0893d060d868
FlorianBEME <florianbme@gmail.com>
2021-11-08 11:00:30 +0100

    ajout gestion d'erreur

--- client/src/components/admin/event/FooterSettings.tsx
+++ client/src/components/admin/event/FooterSettings.tsx
@@ -7,6 +7,7 @@ import { emitEvent, subscribeToSocket } from "../../common/socket";
 import ItemFooterCard from "./footerSettingsComponents/ItemFooterCard";
 import { FaRegCheckSquare } from "react-icons/fa";
 import { AiOutlineDownload } from "react-icons/ai";
+import { removeInput } from "../../common/removeInput";
 
 const MySwal = withReactContent(Swal);
 
@@ -120,16 +121,35 @@ export default function FooterSettings() {
   // modification du copyright
   const modifyCopyright = (e: SyntheticEvent) => {
     e.preventDefault();
-    axios
-      .put(`${FETCH}/copyright/1`, copyright,{
-        headers: {
-          "x-access-token": token,
-        },
-      })
-      .then(() => {})
-      .catch((err) => {
-        console.error(err);
-      });
+    MySwal.fire({
+      title: "Confirmation",
+      text: "Êtes-vous sur de vouloir ajouter ce nouvel icone au pied de page?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Oui, je suis sur !",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .put(`${FETCH}/copyright/1`, copyright, {
+            headers: {
+              "x-access-token": token,
+            },
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              Swal.fire("Modifié!", "", "success");
+              removeInput(['text'])
+            }
+            console.log(res);
+          })
+          .catch((err) => {
+            console.error(err);
+            Swal.fire("Erreur!", "", "error");
+          });
+      }
+    });
   };
 
   useEffect(() => {
@@ -299,6 +319,7 @@ export default function FooterSettings() {
           )}
         </div>
       </div>
+      {/* Modification du texte copyright */}
       <div className="px-4 py-5 sm:p-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-white">
           Modifier le text "copyright"
@@ -338,7 +359,7 @@ export default function FooterSettings() {
                 type="submit"
                 className="mt-4 sm:mt-0 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500  sm:w-auto sm:text-sm"
               >
-                Ajouter
+                Modifier
               </button>
             </div>
           </div>
