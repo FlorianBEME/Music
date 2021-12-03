d75091ac4f1fca89338a704ceeee81a8d50ee1b5
FlorianBEME <florianbme@gmail.com>
2021-12-03 17:35:40 +0100

    ajout ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      script: "server.js",
+      watch: ".",
+    },
+  ],
+  // cd client && npm i && npm run build
+  deploy: {
+    production: {
+      user: "root",
+      host: "195.20.240.170",
+      ref: "origin/main",
+      repo: "https://github.com/FlorianBEME/Music.git",
+      path: "/home/Project",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && cd client && npm install && npm run build& cd ..& pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
